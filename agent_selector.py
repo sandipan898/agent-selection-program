d6be6095be82ce32e27094bed76c11a63445eae3
sandipan <sandipan@localhost.localdomain>
2020-06-22 00:18:30 -0700

    update selection conditions

--- agent_selector.py
+++ agent_selector.py
@@ -1,36 +1,24 @@
-"""
-class AgentSelector():
-    is_available = None
-    available_since = None
-    roles = None
-
-    def __init__(self):
-        pass
-
-    def select_agent(self):
-        pass
-"""
-
-
-agent_list = {
-    'agent_1': {'is_available': 1, 'available_since': 2, 'roles': 'spanish speaker'},
-    'agent_2': {'is_available': 1, 'available_since': 3, 'roles': 'sales'}
-    }
+agent_list = [
+    {'agent_id': 1, 'is_available': 1, 'available_since': 2, 'roles': 'spanish speaker'},
+    {'agent_id': 2, 'is_available': 1, 'available_since': 3, 'roles': 'sales'},
+]
 
 selection_mode_list = ['all available', 'least busy', 'random']
 
 
 def select_agent(agent_list, selection_mode):
+    """ check the conditions based on selection mode and returns the agent id of selected agents """
     
     selected_agent_list = []
     selection_mode = selection_mode.lower()
+    available_time_list = [i['available_since'] for i in agent_list]
 
     if selection_mode == 'all available':
-        selected_agent_list.append(agent_list) 
+        selected_agent_list.append([i['agent_id'] for i in agent_list]) 
     if selection_mode == 'least busy':
-        selected_agent_list.append(agent_list)
+        selected_agent_list.append(max(available_time_list))
     if selection_mode == 'random':
-        selected_agent_list.append(agent_list)
+        selected_agent_list.append([i['agent_id'] for i in agent_list])
 
     return selected_agent_list
 
