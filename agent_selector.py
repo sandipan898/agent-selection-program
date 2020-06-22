f30919420180ac34bab6bac7679fb7543693ed48
sandipan <sandipan@localhost.localdomain>
2020-06-22 07:48:36 -0700

    Update python file

--- agent_selector.py
+++ agent_selector.py
@@ -32,17 +32,18 @@ def select_agent(agent_list, selection_mode, issue_role):
     selection_mode = selection_mode.lower()
     for item in agent_list:
         if item['is_available']:
+            available_time_list = [i['available_since'] for i in agent_list]
 
             if selection_mode == 'all available':
-                selected_agents = [i['agent_id'] for i in agent_list]
-            available_time_list = [i['available_since'] for i in agent_list]
+                selected_agents = [i['agent_id'] for i in agent_list if i['roles'] == issue_role]
 
             if selection_mode == 'least busy':
                 max_time = max(available_time_list)
-                selected_agents = [i['agent_id'] for i in agent_list if i['available_since'] == max_time]
+                selected_agents = [i['agent_id'] for i in agent_list if i['available_since'] == max_time and i['roles'] == issue_role]
 
             if selection_mode == 'random':
-                selected_agents = [random.choice(agent_list)['agent_id']]
+                s = random.choice(agent_list)
+                selected_agents = [s['agent_id'] if s['roles'] == issue_role else None]
 
     return selected_agents
 
@@ -60,11 +61,14 @@ agent_list = [
     {'agent_id': 4, 'is_available': 1, 'available_since': 1, 'roles': 'support'},
     {'agent_id': 5, 'is_available': 1, 'available_since': 3, 'roles': 'sales'},
 ]
+issue_role = ['sales', 'support']
 mode = input("Enter agent selection mode (all available, least busy or random): ")
-selected_agents = select_agent(agent_list, mode)
 
-print("\n*********Selected agents with their data*********\n")
-for a_id in selected_agents:
-    for i in agent_list:
-        if i['agent_id'] == a_id:
-            print("Agent-{}  Available_since: {}  Role: {}".format(a_id, i['available_since'], i['roles']))
+for role in issue_role:
+    selected_agents = select_agent(agent_list, mode, role)
+
+    print("\n*********Selected agents for issue role {} with their data*********\n".format(role))
+    for a_id in selected_agents:
+        for i in agent_list:
+            if i['agent_id'] == a_id:
+                print("Agent-{}  Available_since: {}  Role: {}".format(a_id, i['available_since'], i['roles']))
